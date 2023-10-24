13676431f336532e6c1544ba0021f38d4cbdcebc
oxi-moron <rafaelmagalhaes030903@gmail.com>
2023-10-24 18:17:15 +0100

    minor issues fixed

--- Estudante.cpp
+++ Estudante.cpp
@@ -1,5 +1,13 @@
-//
-// Created by oxi-moron on 24-10-2023.
-//
-
 #include "Estudante.h"
+
+Estudante::Estudante(int id, string name, list<pair<string,string>> classes) {
+    for (pair<string, string> t : classes) {
+        schedule.push_back(Turma(t.first, t.second));
+    }
+}
+
+int const Estudante::getID() {return studentID;}
+string const Estudante::getName() {return studentName;}
+list<Turma> const Estudante::getSchedule() {return schedule;}
+
+void setSchedule(list<Turma> newSchedule) const {schedule = newSchedule;}
--- Estudante.h
+++ Estudante.h
@@ -1,16 +1,25 @@
-//
-// Created by oxi-moron on 24-10-2023.
-//
-
 #ifndef AED2324_PRJ1_G15_ESTUDANTE_H
 #define AED2324_PRJ1_G15_ESTUDANTE_H
 
+#include <string>
+#include <list>
 
+using namespace std;
 
 class Estudante {
+private:
+    int studentID;
+    string studentName;
+    list<Turma> schedule;
+public:
+    Estudante(int id, string name, list<pair<string,string>> classes) : studentID(id), studentName(name);
 
+    int const getID();
+    string const getName();
+    list<Turma> const getSchedule();
+
+    void setSchedule(list<Turma> newSchedule) const;
 };
-
 
 
 #endif //AED2324_PRJ1_G15_ESTUDANTE_H
