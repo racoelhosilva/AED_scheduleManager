53a48571cf58cef943f4a543112a6d16749ab39d
oxi-moron <rafaelmagalhaes030903@gmail.com>
2023-10-30 09:03:27 +0000

    bué útil, bué necessário

---
+++ docs/output/html/search/all_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['estudante_0',['Estudante',['../classEstudante.html',1,'']]],
+  ['estudantealphaascending_1',['EstudanteAlphaAscending',['../structEstudanteAlphaAscending.html',1,'']]],
+  ['estudantealphadescending_2',['EstudanteAlphaDescending',['../structEstudanteAlphaDescending.html',1,'']]],
+  ['estudantenumascending_3',['EstudanteNumAscending',['../structEstudanteNumAscending.html',1,'']]],
+  ['estudantenumdescending_4',['EstudanteNumDescending',['../structEstudanteNumDescending.html',1,'']]]
+];
---
+++ docs/output/html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pedido_0',['Pedido',['../classPedido.html',1,'']]]
+];
